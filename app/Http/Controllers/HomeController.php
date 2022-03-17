7561ee731ff14fc30b33b9387203e8d44653d2db
Vasiliy <kkolyasev1228@gmail.com>
2022-03-17 17:33:30 +0400

    start albom

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class HomeController extends Controller
+{
+    public function index(Request $request)
+    {
+        $response = Http::get('http://ws.audioscrobbler.com/2.0/?method=album.search&album=belive&api_key=57d242cef4c59ff3a3dddb723b00a950&format=json');
+        $albom = $response['results']['albummatches']['album'];
+        $albomInfo = Http::get("http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=57d242cef4c59ff3a3dddb723b00a950&artist={$albom[0]['artist']}&album={$albom[0]['name']}&format=json");
+
+        dump($albomInfo['album']['name']);
+        dump($albomInfo['album']['artist']);
+        dump($albomInfo['album']['wiki']['content']);
+        dump($albomInfo['album']['image'][count($albomInfo['album']['image'])-1]['#text']);
+    }
+}
